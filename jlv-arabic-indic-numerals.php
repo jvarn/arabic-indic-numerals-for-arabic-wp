50e474bf5a7ea7fd1d10ce04e6fec4d86cff093c
Jeremy Varnham <jeremy@adhd.org.sa>
2018-12-12 13:53:51 +0300

    Version bump

--- jlv-arabic-indic-numerals.php
+++ jlv-arabic-indic-numerals.php
@@ -3,7 +3,7 @@
  * Plugin Name: Arabic-Indic Numerals for Arabic Wordpress
  * Plugin URI: https://github.com/jvarn/arabic-indic-numerals-for-arabic-wp
  * Description: Converts numbers in dates into Arabic-Indic numerals
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Jeremy Varnham
  * Author URI: https://abuyasmeen.com/
  * License: GPL2
